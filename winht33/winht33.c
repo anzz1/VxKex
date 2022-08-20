c1a0165085ef0ad8cb7d97ec4589ee8c189d9fc8
Michael Shipley <admin@dotexe.cf>
2022-08-20 15:11:05 -0400

    Add some winhttp stubs

---
+++ winht33/winht33.c
@@ -0,0 +1,25 @@
+#include <Windows.h>
+#include <Shlwapi.h>
+#include <KexDll.h>
+#include <Winhttp.h>
+
+
+DWORD WINAPI WinHttpWebSocketClose( HINTERNET hsocket, USHORT status, void *reason, DWORD len )
+{
+    
+}
+
+DWORD WINAPI WinHttpWebSocketReceive(HINTERNET hWebSocket, PVOID pvBuffer, DWORD dwBufferLength, DWORD *pdwBytesRead, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE *peBufferType)
+{
+	
+}
+
+DWORD WINAPI WinHttpWebSocketSend(HINTERNET hWebSocket, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE eBufferType, PVOID pvBuffer, DWORD dwBufferLength)
+{
+	
+}
+
+HINTERNET WINAPI WinHttpWebSocketCompleteUpgrade(HINTERNET hRequest, DWORD_PTR pContext)
+{
+  
+}
