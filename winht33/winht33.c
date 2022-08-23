9a633b012e1914c8273be944e83f0fee457c960d
Michael Shipley <admin@dotexe.cf>
2022-08-23 16:56:29 -0400

    Refactor winht33

--- winht33/winht33.c
+++ winht33/winht33.c
@@ -3,23 +3,22 @@
 #include <KexDll.h>
 #include <Winhttp.h>
 
-
-DWORD WINAPI WinHttpWebSocketClose( HINTERNET hsocket, USHORT status, void *reason, DWORD len )
-{
+DWORD WINAPI WinHttpWebSocketClose( HINTERNET hsocket, USHORT status, void *reason, DWORD len ) {
     
 }
 
-DWORD WINAPI WinHttpWebSocketReceive(HINTERNET hWebSocket, PVOID pvBuffer, DWORD dwBufferLength, DWORD *pdwBytesRead, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE *peBufferType)
-{
+DWORD WINAPI WinHttpWebSocketReceive(HINTERNET hWebSocket, PVOID pvBuffer, DWORD dwBufferLength, DWORD *pdwBytesRead, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE *peBufferType) {
 	
 }
 
-DWORD WINAPI WinHttpWebSocketSend(HINTERNET hWebSocket, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE eBufferType, PVOID pvBuffer, DWORD dwBufferLength)
-{
+DWORD WINAPI WinHttpWebSocketSend(HINTERNET hWebSocket, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE eBufferType, PVOID pvBuffer, DWORD dwBufferLength) {
 	
 }
 
-HINTERNET WINAPI WinHttpWebSocketCompleteUpgrade(HINTERNET hRequest, DWORD_PTR pContext)
-{
-  
+HINTERNET WINAPI WinHttpWebSocketCompleteUpgrade(HINTERNET hRequest, DWORD_PTR pContext) {
+	
+}
+
+void WINAPI WinHttpEtwSetActivityIdFromHandle(void *a1, char a2, struct _WINHTTP_ETW_INFO *a3) {
+	
 }
