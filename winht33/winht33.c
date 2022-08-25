7a43821636f82a47ee460844af33fee4b33af3e3
Michael Shipley <admin@dotexe.cf>
2022-08-25 00:28:58 -0400

    Replace winhttp stubs with forwarded functions

--- winht33/winht33.c
+++ winht33/winht33.c
@@ -4,21 +4,17 @@
 #include <Winhttp.h>
 
 DWORD WINAPI WinHttpWebSocketClose( HINTERNET hsocket, USHORT status, void *reason, DWORD len ) {
-    
+    return WinHttpCloseHandle(hsocket);
 }
 
 DWORD WINAPI WinHttpWebSocketReceive(HINTERNET hWebSocket, PVOID pvBuffer, DWORD dwBufferLength, DWORD *pdwBytesRead, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE *peBufferType) {
-	
+	return WinHttpReceiveResponse(hWebSocket, pvBuffer);
 }
 
 DWORD WINAPI WinHttpWebSocketSend(HINTERNET hWebSocket, enum WINHTTP_WEB_SOCKET_BUFFER_TYPE eBufferType, PVOID pvBuffer, DWORD dwBufferLength) {
-	
+	return WinHttpSendRequest(hWebSocket, ((LPCWSTR)pvBuffer), dwBufferLength, pvBuffer, 0, 0, NULL);
 }
 
 HINTERNET WINAPI WinHttpWebSocketCompleteUpgrade(HINTERNET hRequest, DWORD_PTR pContext) {
-	
-}
-
-void WINAPI WinHttpEtwSetActivityIdFromHandle(void *a1, char a2, struct _WINHTTP_ETW_INFO *a3) {
-	
+	return WinHttpSetStatusCallback(hRequest, NULL, WINHTTP_CALLBACK_FLAG_SENDREQUEST_COMPLETE, NULL);
 }
